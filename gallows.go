5aa237bd1895bdf732cd58ee7e03eeb4e6edf794
Felix Basinger <basingerfelix17@gmail.com>
2024-09-11 12:57:37 +0200

    First commit. Added game "Gallows"

---
+++ gallows.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func Gallows() string {
+	rand.Seed(time.Now().UnixNano())
+	var letterOfPlayer string
+
+	var choiceOfWord int
+	var gameWord string
+	var words = [...]string{"корыто", "пластик", "енот", "сталактит", "гантеля"}
+
+	choiceOfWord = rand.Intn(5)
+	gameWord = words[choiceOfWord]
+	var wordOfPlayer []string = make([]string, len(gameWord))
+	count := 1
+	countLetters := 0
+	for count <= 5 {
+		arrayGameWord := strings.Split(gameWord, "")
+		fmt.Println("Enter your letter: ")
+		fmt.Scan(&letterOfPlayer)
+
+		if letterOfPlayer == arrayGameWord[countLetters] {
+			wordOfPlayer[countLetters] = letterOfPlayer
+			fmt.Println("The letter", countLetters+1, "is right")
+			countLetters++
+			if countLetters == len(arrayGameWord) {
+				return "You win!"
+			}
+		} else {
+			fmt.Println("The letter", countLetters+1, "is wrong( Try again")
+			count++
+		}
+	}
+	return "You lose("
+}
+
+func main() {
+	fmt.Println(Gallows())
+}
